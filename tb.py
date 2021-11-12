aa314f54d509f4b4bc53903bea26b200dbc1c07d
Dheeraj Singh <dheeraj.student2@gmail.com>
2021-11-13 01:58:10 +0800

    start tensorboard from python

--- tb.py
+++ tb.py
@@ -1,4 +1,6 @@
 from streamlit_tensorboard import st_tensorboard
+import os
 
 def app():
+    os.system('python -m tensorflow.tensorboard --logdir=./logs')
     st_tensorboard(logdir="./logs", port=6006, width=1080)
